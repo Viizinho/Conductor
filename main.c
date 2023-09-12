16b04e9a488f507cd42e82e788c6c2174bb64c3c
Viizinho <joaovitorbeltrao@gmail.com>
2023-09-11 23:51:42 -0300

    save pré alteração nome

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h>
+#include <vector>
+#include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
+
     return 0;
 }
